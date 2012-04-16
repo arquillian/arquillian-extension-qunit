8f664a630e03dc56fafe70020d2637b046b1d0b0
Lukas Fryc <lfryc@redhat.com>
2012-04-16 14:17:29 +0200

    added JUnit suite runner with test case generator

---
+++ src/main/java/org/jboss/arquillian/qunit/junit/TestingClass.java
@@ -0,0 +1,11 @@
+package org.jboss.arquillian.qunit.junit;
+
+import org.junit.Test;
+
+public class TestingClass {
+    
+    @Test
+    public void test() {
+        System.out.println("hahaha");
+    }
+}
---
+++ src/test/java/org/jboss/arquillian/qunit/junit/TestJUnitRunner.java
@@ -0,0 +1,8 @@
+package org.jboss.arquillian.qunit.junit;
+
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitRunner.class)
+public class TestJUnitRunner {
+
+}
